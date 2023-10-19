a47da654a6221d96c1c28fa269a7b9e0bb796b38
Hyeonji Choi <53848798+cascadeffect@users.noreply.github.com>
2023-10-19 17:07:11 +0900

    [level 2] Title: 피로도, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = []
+    for permutation in permutations([i for i in range(len(dungeons))], len(dungeons)):
+        ftg = k
+        cnt = 0
+        for idx in permutation:
+            if ftg >= dungeons[idx][0]:
+                ftg -= dungeons[idx][1]
+                cnt += 1
+        answer.append(cnt)
+    return max(answer)
